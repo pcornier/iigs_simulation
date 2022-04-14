96f87d5a4bcabf2bced9d2f5fa2e147f75c26d23
Alan Steremberg <alan.steremberg@gmail.com>
2022-04-14 11:33:59 -0700

    added graphical verilator

---
+++ vsim/sim.v
@@ -0,0 +1,125 @@
+`timescale 1ns / 1ps
+/*============================================================================
+===========================================================================*/
+
+module emu (
+
+	input clk_sys,
+	input reset,
+	input soft_reset,
+	input menu,
+	input adam,
+	
+	input [31:0] joystick_0,
+	input [31:0] joystick_1,
+	input [31:0] joystick_2,
+	input [31:0] joystick_3,
+	input [31:0] joystick_4,
+	input [31:0] joystick_5,
+	
+	input [15:0] joystick_l_analog_0,
+	input [15:0] joystick_l_analog_1,
+	input [15:0] joystick_l_analog_2,
+	input [15:0] joystick_l_analog_3,
+	input [15:0] joystick_l_analog_4,
+	input [15:0] joystick_l_analog_5,
+	
+	input [15:0] joystick_r_analog_0,
+	input [15:0] joystick_r_analog_1,
+	input [15:0] joystick_r_analog_2,
+	input [15:0] joystick_r_analog_3,
+	input [15:0] joystick_r_analog_4,
+	input [15:0] joystick_r_analog_5,
+
+	input [7:0] paddle_0,
+	input [7:0] paddle_1,
+	input [7:0] paddle_2,
+	input [7:0] paddle_3,
+	input [7:0] paddle_4,
+	input [7:0] paddle_5,
+
+	input [8:0] spinner_0,
+	input [8:0] spinner_1,
+	input [8:0] spinner_2,
+	input [8:0] spinner_3,
+	input [8:0] spinner_4,
+	input [8:0] spinner_5,
+
+	// ps2 alternative interface.
+	// [8] - extended, [9] - pressed, [10] - toggles with every press/release
+	input [10:0] ps2_key,
+
+	// [24] - toggles with every event
+	input [24:0] ps2_mouse,
+	input [15:0] ps2_mouse_ext, // 15:8 - reserved(additional buttons), 7:0 - wheel movements
+
+	// [31:0] - seconds since 1970-01-01 00:00:00, [32] - toggle with every change
+	input [32:0] timestamp,
+
+	output [7:0] VGA_R,
+	output [7:0] VGA_G,
+	output [7:0] VGA_B,
+	
+	output VGA_HS,
+	output VGA_VS,
+	output VGA_HB,
+	output VGA_VB,
+
+	output CE_PIXEL,
+	
+	output	[15:0]	AUDIO_L,
+	output	[15:0]	AUDIO_R,
+	
+	input			ioctl_download,
+	input			ioctl_wr,
+	input [24:0]		ioctl_addr,
+	input [7:0]		ioctl_dout,
+	input [7:0]		ioctl_index,
+	output reg		ioctl_wait=1'b0,
+
+	output [31:0] 		sd_lba[2],
+	output [9:0] 		sd_rd,
+	output [9:0] 		sd_wr,
+	input [9:0] 		sd_ack,
+	input [8:0] 		sd_buff_addr,
+	input [7:0] 		sd_buff_dout,
+	output [7:0] 		sd_buff_din[2],
+	input 			sd_buff_wr,
+	input [9:0] 		img_mounted,
+	input 			img_readonly,
+
+	input [63:0] 		img_size
+
+
+
+);
+wire [15:0] joystick_a0 =  joystick_l_analog_0;
+
+wire UART_CTS;
+wire UART_RTS;
+wire UART_RXD;
+wire UART_TXD;
+wire UART_DTR;
+wire UART_DSR;
+
+
+top top (
+	.reset(reset),
+	.clk_sys(clk_sys)
+);
+/*
+assign VGA_R=R;
+assign VGA_G=G;
+assign VGA_B=B;
+assign VGA_HS=hsync;
+assign VGA_VS=vsync;
+assign VGA_HB=hblank;
+assign VGA_VB=vblank;
+
+assign CE_PIXEL=ce_5m3;
+*/
+
+
+
+
+endmodule
