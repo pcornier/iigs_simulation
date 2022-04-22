2f93e918c098d506859afa116b042ef982ac568c
Alan Steremberg <alan.steremberg@gmail.com>
2022-04-22 15:58:23 -0700

    fixed ram size to match mame

--- top.v
+++ top.v
@@ -44,7 +44,8 @@ iigs core(
 );
 
 `ifdef VERILATOR
-parameter RAMSIZE = 127; // 16x64k = 1MB, max = 127x64k = 8MB
+//parameter RAMSIZE = 127; // 16x64k = 1MB, max = 127x64k = 8MB
+parameter RAMSIZE = 20; // 16x64k = 1MB, max = 127x64k = 8MB
 `else
 parameter RAMSIZE = 2; // 16x64k = 1MB, max = 127x64k = 8MB
 `endif
