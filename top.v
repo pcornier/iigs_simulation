045b959f85411be0f06eab39d0cd4a06f2296f63
Frank Bruno <fbruno@asicsolutions.com>
2022-06-04 09:14:07 -0500

    Updated top.v to get diagnostics to boot.

--- top.v
+++ top.v
@@ -1,4 +1,3 @@
-
 module top(
   input reset,
   input clk_sys,
@@ -139,18 +138,18 @@ assign fastram_ce = (bank < RAMSIZE)  & ( ~rom2_ce | rom_writethrough)  & ~rom1_
 reg slowram_ce;
 
 //wire slot_ce =  bank == 8'h0 && addr >= 'hc400 && addr < 'hc800 && ~is_internal;
-wire slot_ce =  (bank == 8'h0 || bank == 8'h1 || bank == 8'he0 || bank == 8'he1) && addr >= 'hc400 && addr < 'hc800 && ~is_internal;
+wire slot_ce =  (bank == 8'h0 || bank == 8'h1 || bank == 8'he0 || bank == 8'he1) && addr >= 'hc100 && addr < 'hc800 && ~is_internal;
 wire is_internal =   ~SLTROMSEL[addr[10:8]];
 wire is_internal_io =   ~SLTROMSEL[addr[6:4]];
 //wire slot_internalrom_ce =  bank == 8'h0 && addr >= 'hc400 && addr < 'hc800 && is_internal;
-wire slot_internalrom_ce =  (bank == 8'h0 || bank == 8'h1 || bank == 8'he0 || bank == 8'he1) && addr >= 'hc400 && addr < 'hc800 && is_internal;
+wire slot_internalrom_ce =  (bank == 8'h0 || bank == 8'h1 || bank == 8'he0 || bank == 8'he1) && addr >= 'hc100 && addr < 'hc800 && is_internal;
 
 // try to setup flags for traditional iie style slots
 reg [7:0] device_select;
 reg [7:0] io_select;
 
 
-always @(*) 
+always @(*)
 begin
 // shadow
 //Bit 6: I/O Memory, Bit 5: Alternate Display Mode
@@ -160,56 +159,56 @@ begin
 //if (~shadow[6]) $display("UNIMPLEMENTED SHADOW 6");
    // read or write to e0 or e1 -- turn on the slowram
    if ((bank == 8'he0 || bank == 8'he1 ) && ~IO )
-	slowram_ce = 1;
+        slowram_ce = 1;
    //Bit 6: I/O Memory
    //else  if ((bank == 8'h00 || bank == 8'h01) && ~IO && ~shadow[6] && addr >= 'hc000 && addr <= 'hcfff )
    else  if ((bank == 8'h00 || bank == 8'h01) && ~IO && shadow[6] && addr >= 'hc000 && addr <= 'hffff )
-	slowram_ce = 1;
+        slowram_ce = 1;
    //Bit 5: Alternate Display Mode
    else  if (bank == 8'h00 && ~shadow[5] && addr >= 'h0800 && addr <= 'h0bff && ~IO)
-	slowram_ce = 1;
+        slowram_ce = 1;
    //Bit 5 AUX: Alt Display Mode
    else  if (bank == 8'h01 && ~shadow[5] && ~shadow[4] && addr >= 'h0800 && addr <= 'h0bff && ~IO)
-	slowram_ce = 1;
+        slowram_ce = 1;
    //Bit 4: (used in combo)
    //Bit 3,2: Super HiRes or parts or HiRes Page 2
    else  if (bank == 8'h00 && (~shadow[2]  || ~shadow[3] ) && addr >= 'h4000 && addr <= 'h5fff && ~IO)
-	slowram_ce = 1;
+        slowram_ce = 1;
    //Bit 3,2: Super HiRes or parts or HiRes Page 2 and Aux
    else  if (bank == 8'h01 && ((~shadow[2] && ~shadow[4]) || ~shadow[3] ) && addr >= 'h4000 && addr <= 'h5fff && ~IO)
-	slowram_ce = 1;
-   //Bit 3,1: Super HiRes or parts or HiRes Page 1 
+        slowram_ce = 1;
+   //Bit 3,1: Super HiRes or parts or HiRes Page 1
    else  if (bank == 8'h00 && (~shadow[1]  || ~shadow[3] ) && addr >= 'h2000 && addr <= 'h3fff && ~IO)
-	slowram_ce = 1;
+        slowram_ce = 1;
    //Bit 3,1: Super HiRes or parts or HiRes Page 1 and Aux
    else  if (bank == 8'h01 && ((~shadow[1] && ~shadow[4]) || ~shadow[3] ) && addr >= 'h2000 && addr <= 'h3fff && ~IO)
-	slowram_ce = 1;
+        slowram_ce = 1;
    //Bit 0: Alternate Display Mode
    else  if (bank == 8'h00 && ~shadow[0] && addr >= 'h0400 && addr <= 'h07ff && ~IO)
-	slowram_ce = 1;
+        slowram_ce = 1;
    //Bit 0 AUX: Alt Display Mode
    else  if (bank == 8'h01 && ~shadow[0] && ~shadow[4] && addr >= 'h0400 && addr <= 'h07ff && ~IO)
-	slowram_ce = 1;
+        slowram_ce = 1;
    else
-   	slowram_ce =0;
-//   if (bank == 8'h00 
+        slowram_ce =0;
+//   if (bank == 8'h00
 end
 
 
 //always @(posedge clk_sys)
 always @(*)
 begin
-   device_select=8'h0;   
-   io_select=8'h0;   
+   device_select=8'h0;
+   io_select=8'h0;
    if ((bank == 8'h0 || bank == 8'h1 || bank == 8'he0 || bank == 8'he1) && addr >= 'hc090 && addr < 'hc100 && ~is_internal_io)
    begin
 //	   $display("device_select addr[10:8] %x %x ISINTERNAL? ",addr[6:4],din);
-	  device_select[addr[6:4]]=1'b1;
+          device_select[addr[6:4]]=1'b1;
   end
-   if ((bank == 8'h0 || bank == 8'h1 || bank == 8'he0 || bank == 8'he1) && addr >= 'hc400 && addr < 'hc800 && ~is_internal && ~CXROM)
+   if ((bank == 8'h0 || bank == 8'h1 || bank == 8'he0 || bank == 8'he1) && addr >= 'hc100 && addr < 'hc800 && ~is_internal && ~CXROM)
    begin
 //	   $display("io_select addr[10:8] %x din %x HDD_DO %x fastclk %x addr %x RD %x",addr[10:8],din,HDD_DO,fast_clk,addr,we);
-	  io_select[addr[10:8]]=1'b1;
+          io_select[addr[10:8]]=1'b1;
   end
 end
 
@@ -221,7 +220,7 @@ begin
         if (fast_clk)
         begin
                 $display("bank %x addr %x rom1_ce %x rom2_ce %x fastram_ce %x slot_internalrom_ce %x slowram_ce %x slot_ce %x rom2_dout %x din %x SLOTROMSEL %x is_internal %x CXROM %x shadow %x IO %x io_select[7] %x device_select[7] %x raddr %x",
-			bank,addr,rom1_ce,rom2_ce,fastram_ce,slot_internalrom_ce,slowram_ce,slot_ce,rom2_dout,din,SLTROMSEL,is_internal,CXROM,shadow,IO,io_select[7],device_select[7],raddr);
+                        bank,addr,rom1_ce,rom2_ce,fastram_ce,slot_internalrom_ce,slowram_ce,slot_ce,rom2_dout,din,SLTROMSEL,is_internal,CXROM,shadow,IO,io_select[7],device_select[7],raddr);
         end
 end
 
@@ -231,8 +230,8 @@ wire [7:0] din =
   (io_select[7] == 1'b1 | device_select[7] == 1'b1) ? HDD_DO :
   rom1_ce ? rom1_dout :
   rom2_ce ? rom2_dout :
-  fastram_ce ? fastram_dout :
   slot_internalrom_ce ?  rom2_dout :
+  fastram_ce ? fastram_dout :
   slowram_ce ? slowram_dout :
   slot_ce ? slot_dout :
   8'h80;
@@ -280,12 +279,12 @@ dpram #(.widthad_a(23),.prefix("fast")) fastram
 dpram #(.widthad_a(16)) fastram
 `endif
 (
-	.clock_a(clk_sys),
-	.address_a({ bank[6:0], raddr }),
-	.data_a(dout),
-	.q_a(fastram_dout),
-	.wren_a(we),
-	.ce_a(fastram_ce),
+        .clock_a(clk_sys),
+        .address_a({ bank[6:0], raddr }),
+        .data_a(dout),
+        .q_a(fastram_dout),
+        .wren_a(we),
+        .ce_a(fastram_ce),
 );
 
 `endif
@@ -310,21 +309,21 @@ slowram slowram(
 */
 dpram #(.widthad_a(17),.prefix("slow"),.p(" e")) slowram
 (
-	.clock_a(clk_sys),
-	.address_a({ bank[0], raddr }),
-	.data_a(dout),
-	.q_a(slowram_dout),
-	.wren_a(we),
-	.ce_a(slowram_ce),
+        .clock_a(clk_sys),
+        .address_a({ bank[0], raddr }),
+        .data_a(dout),
+        .q_a(slowram_dout),
+        .wren_a(we),
+        .ce_a(slowram_ce),
 
-	.clock_b(clk_vid),
-	.address_b(video_addr[16:0]),
-	.data_b(0),
-	.q_b(video_data),
-	.wren_b(1'b0)
+        .clock_b(clk_vid),
+        .address_b(video_addr[16:0]),
+        .data_b(0),
+        .q_b(video_data),
+        .wren_b(1'b0)
 
 
-	//.ce_b(1'b1)
+        //.ce_b(1'b1)
 );
 
 
@@ -349,25 +348,25 @@ wire [22:0] video_addr;
 wire [7:0] video_data;
 wire vbl_irq;
 vgc vgc(
-	.clk(clk_sys),
-	.clk_vid(clk_vid),
-	.ce_pix(ce_pix),
-	.scanline_irq(scanline_irq),
-	.vbl_irq(vbl_irq),
-	.H(H),
-	.V(V),
-	.R(R),
-	.G(G),
-	.B(B),
-	.video_addr(video_addr),
-	.video_data(video_data),
-	.TEXTCOLOR(TEXTCOLOR),
-	.BORDERCOLOR(BORDERCOLOR),
-	.HIRES_MODE(HIRES_MODE),
-	.PAGE2(PAGE2),
-	.TEXTG(TEXTG),	
-	.MIXG(MIXG),
-	.NEWVIDEO(NEWVIDEO)
+        .clk(clk_sys),
+        .clk_vid(clk_vid),
+        .ce_pix(ce_pix),
+        .scanline_irq(scanline_irq),
+        .vbl_irq(vbl_irq),
+        .H(H),
+        .V(V),
+        .R(R),
+        .G(G),
+        .B(B),
+        .video_addr(video_addr),
+        .video_data(video_data),
+        .TEXTCOLOR(TEXTCOLOR),
+        .BORDERCOLOR(BORDERCOLOR),
+        .HIRES_MODE(HIRES_MODE),
+        .PAGE2(PAGE2),
+        .TEXTG(TEXTG),
+        .MIXG(MIXG),
+        .NEWVIDEO(NEWVIDEO)
 );
 
     hdd hdd(
