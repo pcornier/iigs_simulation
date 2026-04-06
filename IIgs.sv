1ffb32a3d5331b88116ee67a4d5dd733be77bf58
Steven A. Wilson <11664974+steven-a-wilson@users.noreply.github.com>
2026-04-05 19:17:49 -0600

    iigs: Move system ROM to SDRAM

--- IIgs.sv
+++ IIgs.sv
@@ -242,6 +242,10 @@ wire  [7:0] paddle_0;
 wire [10:0] ps2_key;
 wire [24:0] ps2_mouse;
 
+wire ioctl_download;
+wire ioctl_wr;
+wire [26:0] ioctl_addr;
+wire [7:0] ioctl_dout;
 
 hps_io #(.CONF_STR(CONF_STR),.VDNUM(4)) hps_io
 (
@@ -266,8 +270,6 @@ hps_io #(.CONF_STR(CONF_STR),.VDNUM(4)) hps_io
 	
 	.TIMESTAMP(TIMESTAMP),
 	
-
-	
 	.buttons(buttons),
 	.status(status),
 	.status_menumask({status[5]}),
@@ -277,7 +279,13 @@ hps_io #(.CONF_STR(CONF_STR),.VDNUM(4)) hps_io
 	.joystick_0(joystick_0),
 	.joystick_l_analog_0(joystick_l_analog_0),
 	.joystick_l_analog_1(joystick_l_analog_1),
-	.paddle_0(paddle_0)
+	.paddle_0(paddle_0),
+
+	.ioctl_download(ioctl_download),
+	.ioctl_wr(ioctl_wr),
+	.ioctl_addr(ioctl_addr),
+	.ioctl_dout(ioctl_dout)
+
 );
 
 ///////////////////////   CLOCKS   ///////////////////////////////
@@ -389,7 +397,7 @@ iigs iigs (
 	.floppy35_motor_on(floppy35_motor_on),
 	.top_addr(addr_bus),
 	.rom_bankaddr(rom_bankaddr),
-	.top_din(iigs_din),
+	.top_din(sdram_dout),
 	.top_dout(iigs_dout),
 	.we(we),
 	.fastram_ce(fastram_ce),
@@ -421,11 +429,11 @@ iigs iigs (
 );
 
 wire [23:0] addr_bus;
+wire [23:0] sdram_addr;
+wire [7:0] sdram_din;
 wire [1:0] rom_bankaddr;
 wire [7:0] iigs_dout;
-wire [7:0] iigs_din;
 wire [7:0] sdram_dout;
-wire [7:0] rom_dout;
 wire we;
 wire fastram_ce;
 wire rom_ce;
@@ -433,8 +441,18 @@ wire fast_clk;
 wire fast_clk_delayed;
 wire fast_clk_delayed_mem;
 
-assign iigs_din = rom_ce ? rom_dout : sdram_dout;
+`ifdef ROM3
+assign sdram_addr = ioctl_download ? {6'b111111, ioctl_addr[17:0]} :
+                    (rom_ce & ~we) ? {6'b111111, rom_bankaddr, addr_bus[15:0]} :
+                    {1'b0, addr_bus[22:0]};
+`else
+assign sdram_addr = ioctl_download ? {7'b1111111, ioctl_addr[16:0]} :
+                    (rom_ce & ~we) ? {7'b1111111, rom_bankaddr[0], addr_bus[15:0]} :
+                    {1'b0, addr_bus[22:0]};
 
+`endif
+
+assign sdram_din = ioctl_download ? ioctl_dout : iigs_dout;
 logic [7:0] ram_data;
 /*
 dpram #(.widthad_a(23),.prefix("fast")) fastram
@@ -499,7 +517,6 @@ sdram sdram
   	.ch2_busy()          // Unconnected
   );
   */
- 
 
  sdram sdram
   (
@@ -518,36 +535,14 @@ sdram sdram
   	.clk(clk_sys),
 
   	// Channel 0: CPU fast RAM
-	.addr({1'b0, addr_bus}),	 // Pad to 25 bits
-	.oe(phi2 & ~we & fastram_ce),
-	.we(phi2 & we & fastram_ce),
-	.din(iigs_dout),
+	.addr(sdram_addr),
+	.oe((phi2 & ~we & (fastram_ce | rom_ce))),
+	.we(((phi2 & we & fastram_ce) | ioctl_wr)),
+	.din(sdram_din),
 	.dout(sdram_dout),
-	.ds(2'b11),
-
-
+	.ds(2'b11)
   );
 
-`ifdef ROM3
-
-rom #(.AW(18), .memfile("rom3/boot.rom")) rom(
-  .clock(clk_sys),
-  .address({rom_bankaddr, addr_bus[15:0]}),
-  .q(rom_dout),
-  .ce(rom_ce)
-);
-
-`else
-
-rom #(.AW(17), .memfile("rom1/boot.rom")) rom(
-  .clock(clk_sys),
-  .address({rom_bankaddr[0], addr_bus[15:0]}),
-  .q(rom_dout),
-  .ce(rom_ce)
-);
-`endif
-  
- 
 /*
 
 //wire ch0_busy = 1'b0;
